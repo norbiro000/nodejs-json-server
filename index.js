b5162df6509d7842445a8a32317cd7fe1ee4e754
norbiro000 <boomtingtong@hotmail.com>
2017-03-02 01:22:57 +0700

    Function load_album_list can filter only diractory

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
+// Auther: Norbiro000
 const http = require('http')
 const fs = require('fs')
 const Promise = require('bluebird')
 
 function load_album_list() {
-  //Return Promise
+  // 1. Return Promise
   return new Promise((resolve, reject) => {
     // This function will read all file in dir
     fs.readdir(
-      // Read file in dir name "album"
+      // 2. Read file in dir name "album"
       'album',
       // Call Back function when file read is done.
       function(err, files){
         if (err) {
-          // if has some error return Promise Reject
+          // 3. if has some error return Promise Reject
           reject(err)
           return
         }
-        // If everthing is okay return Promise resolve with all of files name
-        resolve(files)
+        // 3. files => list of all of files
+        // Now we have some file but not khow whice is real directory
+        const only_dir = [];
+        // 4. Use Iterator for Async Loop 
+        (function iterator (index){
+          if (index == files.length) {
+            // 5. if End of loop will return Dir name list to next step
+            resolve(only_dir)
+            return
+          } else {
+            // 5. if in process we'll check this file is diractory
+            // via fs.stat and isDirectory()
+            fs.stat(
+              "album/" + files[index],
+              function(err, stats){
+                if (err) {
+                  // 6. Has some Error force end of loop
+                  reject(err)
+                  return
+                }
+
+                if (stats.isDirectory()) {
+                  // 6. Append dir name into Array
+                  only_dir.push(files[index])
+                }
+                // 7. recursive with next index
+                iterator(index + 1)
+            })
+          }
+        })(0) // 0. start the Iterator function with index 0
       }
     )
   })
@@ -32,10 +61,10 @@ function handle_income_request (req, res) {
       data: {albums: albums}
     }
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(output + "\n"))
+    res.end(JSON.stringify(output) + "\n")
   }).catch(error => {
     res.writeHead(503, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(error + "\n"))
+    res.end(JSON.stringify(error) + "\n")
     return
   })
 }
